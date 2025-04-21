f2779381e3f6674f119b985cdcf2dffb4364e0ea
Azzy-Dev <118063625+Arzianghanchi@users.noreply.github.com>
2025-04-22 04:57:38 +0530

    aTest (#3)
    final commit message

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
-provider "null" {}
+provider "aws" {
+  region = "us-east-1"
+}
 
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "echo Hello from Digger! and testtttttt"
-  }
+module "s3" {
+  source  = "clouddrove/s3/aws"
+  version = "2.0.0"
+
+  name        = "clouddrove-digger"
+  environment = "test"
+  label_order = ["environment", "name"]
+  versioning  = false
 }
